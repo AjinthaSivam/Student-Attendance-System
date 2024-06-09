950ffacc51400660ebd944eaebedc72574c89006
AjinthaSivam <ajinthasivam2019@gmail.com>
2024-06-09 21:44:35 +0530

    #4.2, added handle logout

--- attendance-system/client/src/pages/Home.js
+++ attendance-system/client/src/pages/Home.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import "../styles/Home.css"
+import "../service.js"
+import service from '../service.js';
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        service.logout();
+        navigate("/");
+    };
   return (
     <div className="container">
         <div className="content">
             <h2>Welcome! 😊</h2>
             <p>We're thrilled to have you back! Let's make today a fantastic learning adventure. 🎉</p>
-            <button>Logout</button>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     </div>
   )
