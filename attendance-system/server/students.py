5a081608f1ec6dbee00496d77a84de6669bbd90a
AjinthaSivam <ajinthasivam2019@gmail.com>
2024-06-09 19:13:56 +0530

    #3.1, created student model

---
+++ attendance-system/server/students.py
@@ -0,0 +1,25 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+students = {
+    "harry": {
+        "username": "harry",
+        "hashed_passwrod": pwd_context.hash("harry123")
+    },
+    "john": {
+        "username": "john",
+        "hashed_passwrod": pwd_context.hash("john123")
+    },
+    "maria": {
+        "username": "maria",
+        "hashed_passwrod": pwd_context.hash("maria123")
+    }
+}
+
+def verify_password(password, hashed_password):
+    return pwd_context.verify(password, hashed_password)
+
+def get_student(username: str):
+    student = students.get(username)
+    return student
